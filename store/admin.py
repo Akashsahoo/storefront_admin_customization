b1d6f8d0976aa0589f0b549c6ca0804fd8914dbb
akash sahoo <akashsahoo15595@gmail.com>
2024-12-15 19:48:59 +0530

    admin panel customize'

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin,messages
+
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html,urlencode
-from .models import Collection,Product,Customer,Order
+from .models import Collection,Product,Customer,Order,OrderItem
+from tags.models import TaggedItem
 # Register your models here.
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -21,9 +23,22 @@ class InventoryFilter(admin.SimpleListFilter):
 
 
 
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    # fields = ['title','slug']
+    # exclude = ['promotions']
+    #readonly_fields = ['title']
+    # prepopulated_fields = {
+    #     'slug': ['title']  inlines = [TagInline]
+    # }
+
+    autocomplete_fields = ['collection']
+    actions = ['clear_inventory']
+
     list_display = ['title','unit_price','inventory_status','collection_title']
+    search_fields = ['title']
     list_editable = ['unit_price']
     list_filter = ['collection','last_update',InventoryFilter]
     list_per_page = 10
@@ -37,6 +52,14 @@ class ProductAdmin(admin.ModelAdmin):
             return 'LOW'
         return 'OK'
 
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self,request,queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were sucessfully updated'
+        )
+
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -46,15 +69,32 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name','last_name']
     search_fields = ['first_name__istartswith','last_name__istartswith']
 
+
+
+
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    autocomplete_fields = ['product']
+    extra = 0
+    min_num = 1
+    max_num = 10
+
+
+
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id','placed_at','customer']
-
+    inlines = [OrderItemInline]
+    autocomplete_fields = ['customer']
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title','products_count']
-
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self,collection):
